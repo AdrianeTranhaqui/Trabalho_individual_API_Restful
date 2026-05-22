053e1798d221f69039ed75133b2376a72fd12308
Adriane Tranhaqui <tranhaquia@gmail.com>
2026-05-21 21:27:57 -0300

    feat: adicionando enum Escolaridade

---
+++ src/main/java/org/serratec/cursos/Enum/Escolaridade.java
@@ -0,0 +1,14 @@
+package org.serratec.cursos.Enum;
+
+public enum Escolaridade {
+
+	Ensino_Fundamental_Incompleto,
+    Ensino_Fundamental_Completo,
+    Ensino_Medio_Incompleto,
+    Ensino_Medio_Completo,
+    Ensino_Superior_Incompleto,
+    Ensino_Superior_Completo,
+    Pos_Graduacao,
+    Mestrado,
+    Doutorado
+}
